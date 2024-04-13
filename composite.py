ecfb852cd9e8a18b3d3718c7004552f363897c4d
Alex Redwood <aredwood@users.noreply.github.com>
2024-04-13 15:54:12 +1000

    functioning video overlay

--- composite.py
+++ composite.py
@@ -1,6 +1,48 @@
 #!/usr/bin/python3
 import os
 import subprocess
+import ffmpeg
+
+def resize_image(image_path, dimensions, new_file):
+    subprocess.run([
+        # magick convert ./memories/A-overlay.png -resize 998x1920 new.png
+        "magick",
+        "convert",
+        image_path,
+        "-resize",
+        str(dimensions["width"]) + "x" + str(dimensions["height"]),
+        new_file
+    ])
+
+def get_image_size(image_path):
+    result = subprocess.run([
+        "magick",
+        "identify",
+        "-format",
+        "%w %h",
+        image_path
+    ],capture_output=True)
+
+    result_string = result.stdout.decode("utf8")
+
+    width, height = result_string.split(" ")
+
+    return {"height":int(height), "width":int(width)}
+
+
+def composite_images(overlay_path, main_path, new_file):
+    # magick composite new.png ./memories/A-main.jpg -compose over -gravity center composite.png
+    subprocess.run([
+        "magick",
+        "composite",
+        overlay_path,
+        main_path,
+        "-compose",
+        "over",
+        "-gravity",
+        "center",
+        new_file
+    ])
 
 # list the files in the memories folder
 memories_files = os.listdir("./memories")
@@ -33,58 +75,80 @@ for overlay in overlays:
 
     jpg_exists = os.path.isfile("./memories/" + memory_id + "-main.jpg")
 
-    # if (mp4_exists and jpg_exists) or (not mp4_exists and not jpg_exists):
-    #     raise Exception("SOMETHING_IS_VERY_WRONG")
+    if (mp4_exists and jpg_exists) or (not mp4_exists and not jpg_exists):
+        raise Exception("SOMETHING_IS_VERY_WRONG")
+
+    overlay_path = f"./memories/{memory_id}-overlay.png"
 
     # we need to use ffmpeg to apply the overlay to a video
     if mp4_exists:
-        pass
+
+        mp4_path = f"./memories/{memory_id}-main.mp4"
+
+        # get the size of the video
+        mp4_probe = ffmpeg.probe(mp4_path)
+        
+        h264_streams = [stream for stream in mp4_probe["streams"] if stream['codec_name'] == 'h264']
+
+        if (len(h264_streams) != 1):
+            raise Exception("UNABLE_TO_FIND_STREAM")
+
+        mp4_stream = h264_streams[0];
+
+        # we need to check display matrix to get the rotation of the video, 
+        # so we know whether its height x width or width x height.
+        # if this fails, just assume the video is rotated.
+        rotated = True
+        try:
+            display_matrix = [side_data for side_data in mp4_stream["side_data_list"] if side_data["side_data_type"] == "Display Matrix"][0]
+
+            rotation = display_matrix["rotation"]
+
+            # if we can get the display matrix, 
+            # check if its rotated by 90 deg, 
+            # 180 deg doesn't matter because the dimensions remain the same
+            rotated = abs(rotation / 90) % 2 == 1
+        except:
+            pass;
+
+
+
+        if rotated:
+            mp4_dimemsions = {
+                "height":mp4_stream["coded_width"],
+                "width":  mp4_stream["coded_height"],
+            }
+        else:
+            mp4_dimemsions = {
+                "height": mp4_stream["coded_height"],
+                "width": mp4_stream["coded_width"]
+            }
+
+
+        print(mp4_dimemsions)
+
+        overlay_path_tmp = overlay_path + ".tmp.png"
+
+
+
+        # resize the overlay
+        resize_image(overlay_path,mp4_dimemsions,overlay_path_tmp)
+
+        # apply the overlay to the video
+        ffmpeg.filter(
+            [
+                ffmpeg.input(mp4_path),
+                ffmpeg.input(overlay_path_tmp)
+            ],
+            'overlay'
+        ).output(f"./memories/{memory_id}-composite.mp4",loglevel="quiet").overwrite_output().run()
+
+
+        os.remove(overlay_path_tmp)
 
     # we need to use image magick to apply the overlay to a picture
     if jpg_exists:
-
-        overlay_path = f"./memories/{memory_id}-overlay.png"
         main_path = f"./memories/{memory_id}-main.jpg"
-
-        def get_image_size(image_path):
-            result = subprocess.run([
-                "magick",
-                "identify",
-                "-format",
-                "%w %h",
-                image_path
-            ],capture_output=True)
-
-            result_string = result.stdout.decode("utf8")
-
-            width, height = result_string.split(" ")
-
-            return {"height":int(height), "width":int(width)}
-        
-        def resize_image(image_path, dimensions, new_file):
-            subprocess.run([
-                # magick convert ./memories/A-overlay.png -resize 998x1920 new.png
-                "magick",
-                "convert",
-                image_path,
-                "-resize",
-                str(dimensions["width"]) + "x" + str(dimensions["height"]),
-                new_file
-            ])
-        
-        def composite_images(overlay_path, main_path, new_file):
-            # magick composite new.png ./memories/A-main.jpg -compose over -gravity center composite.png
-            subprocess.run([
-                "magick",
-                "composite",
-                overlay_path,
-                main_path,
-                "-compose",
-                "over",
-                "-gravity",
-                "center",
-                new_file
-            ])
 
         overlay_size = get_image_size(overlay_path)
         main_size = get_image_size(main_path)
